c6a541f25d94625578a1902ce94dcd813cc229fe
JErBerlin <jeronirm@gmail.com>
2020-02-14 23:07:29 +0100

    post new message with automatic generated time and id

--- ginserver.go
+++ ginserver.go
@@ -5,9 +5,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
+//TODO: refactor some the decode, write and read functionality out the of the gin server file to separate purposes
 func getHomePage(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Welcome to the back message board. You can authenticate at /admin, " +
 		"query one message by id at /view/:id, " +
@@ -19,29 +22,19 @@ func postMessage(c *gin.Context) {
 	body := c.Request.Body
 	value, err := ioutil.ReadAll(body)
 	if err!= nil {
-		c.JSON(http.StatusUnprocessableEntity , gin.H{"message": "the post requested was invalid"})
+		c.JSON(http.StatusUnprocessableEntity , gin.H{"message": "the post request is invalid"})
 		return
 	}
-
 	oneMessage := Message{}
 	json.Unmarshal(value, &oneMessage)
 
-	/*
-	form := "2006-01-02T15:04:05-07:00"
-	t, err := time.Parse(form, record[4])
-	if err!= nil {
-		c.JSON(http.StatusUnprocessableEntity , gin.H{"message": "the post requested was invalid"})
-		return
-	}
+	newTime := time.Now()
+	newSource := rand.NewSource(int64(newTime.Nanosecond()))
+	randNow := rand.New(newSource)
+	newIdStr, _ := randomIdStr16(randNow)
+	oneMessage.Id = newIdStr
+	oneMessage.CreationTime = newTime
 
-	oneMessage := Message{
-		Id: value.id,
-		Name: value.name,
-		Email: value.email,
-		Text: value.text,
-		CreationTime: t,
-	}
-	*/
 	err = writeMessageToFile(oneMessage, PathToMessagesFile)
 	if err!= nil {
 		c.JSON(http.StatusInternalServerError , gin.H{"message": "the post requested could not be made"})
@@ -50,7 +43,7 @@ func postMessage(c *gin.Context) {
 	c.JSON(http.StatusOK , gin.H{"message": "message was posted successfully"})
 }
 
-
+//TODO: do we need query method apart from view message by path?
 func messageQuery (c *gin.Context) {
 	pathToFile := PathToMessagesFile
 	mapIdPos, err := fillPositionIndex(pathToFile)
@@ -98,9 +91,9 @@ func viewOneMessageByPath (c *gin.Context) {
 func startServing() {
 	r := gin.Default()
 	r.GET("/", getHomePage)
-	r.GET("/query", messageQuery)
+	r.GET("/query", messageQuery)	// could have different functionality than view
 	r.GET("/view/:id", viewOneMessageByPath)
 	r.POST("/new", postMessage)
-	r.GET("/messages", getHomePage)
+	r.GET("/messages", getHomePage)  // not functional yet, should be streaming for big files
 	r.Run() // listen and serve on 0.0.0.0:8080 ("localhost:8080")
 }
